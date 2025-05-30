f5a296238ac15772f1e32d85a82a2b5f68ce393d
Siddharth Vaish <sidvaish97@gmail.com>
2025-05-30 17:38:57 +0530

    Address comments

--- utils/configs/globalSetup.ts
+++ utils/configs/globalSetup.ts
@@ -1,15 +1,6 @@
 import * as dotenv from 'dotenv';
-import {
-  APIRequestContext,
-  Page,
-  PlaywrightWorkerArgs,
-  WorkerFixture,
-  request,
-  test as base
-}
-  from '@playwright/test';
 
 dotenv.config();
 
-export const O3_URL = `${process.env.TEST_ENVIRONMENT}` == 'qa' ? `${process.env.O3_URL_QA}` : `${process.env.O3_URL_DEV}`;
-export const KEYCLOAK_URL = `${process.env.TEST_ENVIRONMENT}` == 'qa' ? `${process.env.KEYCLOAK_URL_QA}` : `${process.env.KEYCLOAK_URL_DEV}`;
+export const O3_URL = `${process.env.O3_URL}`;
+export const KEYCLOAK_URL = `${process.env.KEYCLOAK_URL}`;
